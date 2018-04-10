f0957dfcc236fcadeb44704c2ee95cbbb43e4102
Grigoriy Vevyurko <grivurko@yandex.ru>
2018-04-10 04:48:12 -0400

    changed testing for using it in  pytest

--- tests.py
+++ tests.py
@@ -1,30 +1,26 @@
 from maxima import find_maxima
-import unittest
-class Test_maxima(unittest.TestCase):
-    def test_ez1(self):
-        x = [1, 2, 1, 3, 1, 4]
-        ans = [1, 3, 5]
-        self.assertEqual(find_maxima(x), ans)
 
-    def test_ez2(self):
-        x = [1, 2, 5, 7, 4, 5]
-        ans = [3, 5]
-        self.assertEqual(find_maxima(x), ans)
+def test_ez1():
+    x = [1, 2, 1, 3, 1, 4]
+    ans = [1, 3, 5]
+    assert find_maxima(x) == ans
 
-    def test_sided(self):
-        x = [1, 5]
-        ans = [1]
-        self.assertEqual(find_maxima(x), ans)
+def test_ez2():
+    x = [1, 2, 5, 7, 4, 5]
+    ans = [3, 5]
+    self.assertEqual(find_maxima(x), ans)
 
-    def test_nomaxima(self):
-        x = [2, 1]
-        ans = []
-        self.assertEqual(find_maxima(x), ans)
+def test_sided():
+    x = [1, 5]
+    ans = [1]
+    assert find_maxima(x) == ans
 
-    def test_equalvals(self):
-        x = [1, 2, 2, 1 ,4, 4]
-        ans = [5]
-        self.assertEqual(find_maxima(x), ans)
+def test_nomaxima():
+    x = [2, 1]
+    ans = []
+    assert find_maxima(x) == ans
 
-if __name__ == '__main__':
-    unittest.main()
+def test_equalvals():
+    x = [1, 2, 2, 1 ,4, 4]
+    ans = [5]
+    assert find_maxima(x) == ans
