7c22b9b11ac471843a313861a70272c300239538
Grigoriy Vevyurko <grivurko@yandex.ru>
2018-04-10 04:59:59 -0400

    fixed find maxima function and some typos in tests

--- maxima.py
+++ maxima.py
@@ -12,10 +12,21 @@ def find_maxima(x):
     Output:
         idx -- list of indices of the local maxima in x
     """
+    if len(x) < 1:
+        return []
+
+    if len(x) == 1:
+        return [0]
 
     idx = []
-    for i in range(len(x)):
+    if x[0] > x[1]:
+        idx.append(0)
+
+    for i in range(1, len(x) - 1):
         # `i` is a local maximum if the signal decreases before and after it
         if x[i-1] < x[i] and x[i+1] < x[i]:
             idx.append(i)
+
+    if x[-1] > x[-2]:
+        idx.append(len(x) - 1)
     return idx
--- tests.py
+++ tests.py
@@ -8,19 +8,24 @@ def test_ez1():
 def test_ez2():
     x = [1, 2, 5, 7, 4, 5]
     ans = [3, 5]
-    self.assertEqual(find_maxima(x), ans)
+    assert find_maxima(x) == ans
 
 def test_sided():
     x = [1, 5]
     ans = [1]
     assert find_maxima(x) == ans
 
-def test_nomaxima():
+def test_sided2():
     x = [2, 1]
+    ans = [0]
+    assert find_maxima(x) == ans
+
+def test_nomaxima():
+    x = [1, 2, 3, 4, 4]
     ans = []
     assert find_maxima(x) == ans
 
 def test_equalvals():
-    x = [1, 2, 2, 1 ,4, 4]
-    ans = [5]
+    x = [1, 2, 2, 1 ,4, 1]
+    ans = [4]
     assert find_maxima(x) == ans
